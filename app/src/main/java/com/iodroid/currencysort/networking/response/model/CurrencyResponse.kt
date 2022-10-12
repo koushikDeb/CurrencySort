bf993118aaf51d578eb745bfe44b847fd830d795
Koushik Deb <koushik.iodroid@gmail.com>
2022-10-12 20:43:18 +0530

    Feat: Response Models Removed

--- app/src/main/java/com/iodroid/currencysort/networking/response/model/CurrencyResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.iodroid.currencysort.networking.response.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CurrencyResponse(
-    var date: String? = null,
-    var aud: Aud? = Aud()
-)
